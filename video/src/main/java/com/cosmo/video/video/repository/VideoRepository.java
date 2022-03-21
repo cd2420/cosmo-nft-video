ca3a2ad060105654d0ed0110291c0086b42ea337
imdaegeun <cd3818@naver.com>
2022-03-21 23:53:54 +0900

    - push 되는지 테스트

--- video/src/main/java/com/cosmo/video/video/repository/VideoRepository.java
+++ video/src/main/java/com/cosmo/video/video/repository/VideoRepository.java
@@ -3,5 +3,8 @@ package com.cosmo.video.video.repository;
 import com.cosmo.video.video.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    Optional<Video> findById(Long id);
 }
