b19b2d787776c441f06d93eca7c6d9ce09a22c7f
imdaegeun <cd3818@naver.com>
2022-03-22 21:19:04 +0900

    - 해당 모듈 application.yml파일에 view 파일 찾는 설정 셋팅 - Controller에서 view 접근하는 샘플 코드 추가

--- video/src/main/java/com/cosmo/video/video/controller/VideoAPIController.java
+++ video/src/main/java/com/cosmo/video/video/controller/VideoAPIController.java
@@ -2,19 +2,20 @@ package com.cosmo.video.video.controller;
 
 import com.cosmo.video.video.service.VideoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/video")
+@RestController
+@RequestMapping("/api/video")
 @RequiredArgsConstructor
-public class VideoController {
+public class VideoAPIController {
 
     private final VideoService videoService;
 
     @GetMapping("/hello")
-    public String helloWorld() {
-        return "index";
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok(videoService.helloWorld());
     }
 }
