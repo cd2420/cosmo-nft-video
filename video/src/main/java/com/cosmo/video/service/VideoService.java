ac61ab324280d55dae1034004e51dc78ab586102
imdaegeun <cd3818@naver.com>
2022-03-28 07:22:04 +0900

    - eureka를 사용해서 nftServer와 rest통신 설정

--- video/src/main/java/com/cosmo/video/service/VideoService.java
+++ video/src/main/java/com/cosmo/video/service/VideoService.java
@@ -1,6 +1,10 @@
 package com.cosmo.video.service;
 
+import com.cosmo.video.dto.NftDto;
+
 public interface VideoService {
 
     public String helloWorld();
+
+    public NftDto getNft(Long nftId);
 }
