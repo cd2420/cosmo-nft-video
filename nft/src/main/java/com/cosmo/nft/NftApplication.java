4002c86fb5349d268b823405c7c308a4bed8d671
imdaegeun <cd3818@naver.com>
2022-03-27 13:38:59 +0900

    - video, nft server의 외부 설정을 confsvr에서 가져오기

--- nft/src/main/java/com/cosmo/nft/NftApplication.java
+++ nft/src/main/java/com/cosmo/nft/NftApplication.java
@@ -2,11 +2,13 @@ package com.cosmo.nft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-public class Application {
+@RefreshScope
+public class NftApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(NftApplication.class, args);
     }
 }
--- video/src/main/java/com/cosmo/video/VideoApplication.java
+++ video/src/main/java/com/cosmo/video/VideoApplication.java
@@ -2,11 +2,13 @@ package com.cosmo.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-public class Application {
+@RefreshScope
+public class VideoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(VideoApplication.class, args);
     }
 }
