7815624b30b6dd66875d74238238e0917bbc21bf
imdaegeun <cd3818@naver.com>
2022-04-02 20:32:43 +0900

    - client 회복성 패턴 구현 [HystrixCommand] 작동 확인

--- video/src/main/java/com/cosmo/video/controller/VideoAPIController.java
+++ video/src/main/java/com/cosmo/video/controller/VideoAPIController.java
@@ -2,7 +2,10 @@ package com.cosmo.video.controller;
 
 import com.cosmo.video.dto.NftDto;
 import com.cosmo.video.service.VideoService;
+import com.cosmo.video.utils.UserContextHolder;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/api/video")
 @RequiredArgsConstructor
 public class VideoAPIController {
+    private static final Logger logger = LoggerFactory.getLogger(VideoAPIController.class);
 
     private final VideoService videoService;
 
@@ -23,5 +27,12 @@ public class VideoAPIController {
     @GetMapping("/nft")
     public ResponseEntity<NftDto> getNft() {
         return ResponseEntity.ok(videoService.getNft(1L));
+    }
+
+    @GetMapping("/hystrix")
+    public ResponseEntity<Void> getTestHystrix() {
+        logger.info("VideoAPIController Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+        videoService.testHystrix();
+        return ResponseEntity.ok().build();
     }
 }
--- video/src/main/java/com/cosmo/video/service/impl/VideoServiceImpl.java
+++ video/src/main/java/com/cosmo/video/service/impl/VideoServiceImpl.java
@@ -5,24 +5,25 @@ import com.cosmo.video.config.VideoConfig;
 import com.cosmo.video.dto.NftDto;
 import com.cosmo.video.repository.VideoRepository;
 import com.cosmo.video.service.VideoService;
+import com.cosmo.video.utils.UserContextHolder;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class VideoServiceImpl implements VideoService {
+    private static final Logger logger = LoggerFactory.getLogger(VideoServiceImpl.class);
 
     private final VideoRepository videoRepository;
     private final VideoConfig videoConfig;
     private final NftRestTemplateClient nftRestTemplateClient;
-
-    private NftDto retrieveOrgInfo(Long nftId, String clientType){
-        System.out.println("I am using the rest client");
-        NftDto nftDto = nftRestTemplateClient.getNft(nftId);
-        return nftDto;
-    }
-
 
     @Override
     public String helloWorld() {
@@ -31,9 +32,43 @@ public class VideoServiceImpl implements VideoService {
 
     @Override
     @Transactional(readOnly = true)
+    @HystrixCommand
     public NftDto getNft(Long nftId) {
-        NftDto nftDto = retrieveOrgInfo(nftId, "discovery");
-
-        return nftDto;
+        return  nftRestTemplateClient.getNft(nftId);
     }
+
+    @Override
+    @HystrixCommand(//fallbackMethod = "buildFallbackLicenseList",
+            threadPoolKey = "licenseByOrgThreadPool",
+            threadPoolProperties =
+                    {@HystrixProperty(name = "coreSize",value="30"),
+                            @HystrixProperty(name="maxQueueSize", value="10")},
+            commandProperties={
+                    @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value="10"),
+                    @HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value="75"),
+                    @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value="7000"),
+                    @HystrixProperty(name="metrics.rollingStats.timeInMilliseconds", value="15000"),
+                    @HystrixProperty(name="metrics.rollingStats.numBuckets", value="5")}
+    )
+    public void testHystrix() {
+        logger.info("LicenseService.getLicensesByOrg  Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+        randomlyRunLong();
+    }
+
+    private void randomlyRunLong(){
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+
+        if (randomNum==3) sleep();
+    }
+
+    private void sleep(){
+        try {
+            Thread.sleep(11000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
